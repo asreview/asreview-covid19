e3df40d600aafc03af8c7e4e2b110a7b3f1e8a42
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-03-30 11:25:26 +0200

    Update SHA for V5

--- asreviewcontrib/covid19/datasets.py
+++ asreviewcontrib/covid19/datasets.py
@@ -20,17 +20,14 @@ class Cord19Dataset(BaseDataSet):
 class Cord19DatasetV5(Cord19Dataset):
     dataset_id = "cord19-v5"
     title = "CORD-19 v5"
-    sha512 = ("517e2399767aa1d387baaa07c42ef6ac9a5aec1e3a41f832974ee712413"
-              "272429f2a5ea658b32bb7330becac70df1ee5262ae1ddebb258a02aaaa2"
-              "d4b47335cc")
-
     date = "2020-03-27"
     statistics = {
         "n_papers": 45774,
         "n_missing_titles": 157,
         "n_missing_abstracts": 7861,
     }
-    url = ("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-27/metadata.csv")
+    url = ("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-27/metadata.csv")  # noqa
+    sha512 = ("a6d8e7a4dd3f0194840129b7368fd97354b44a44fc0d06addfc9a27f3c08e19ef88ff568effb92994895353f7e9d771932f354bd619ee2242d4262f7561f41fe")  # noqa
 
 
 class Cord19DatasetV5_Dec2019(Cord19Dataset):
@@ -43,12 +40,8 @@ class Cord19DatasetV5_Dec2019(Cord19Dataset):
         "n_missing_abstracts": 877,
     }
     date = "2020-03-30"
-    url = ("https://raw.githubusercontent.com/asreview/"
-           "asreview-covid19/master/datasets/"
-           "cord19_v5_20191201.csv")
-    sha512 = ("7943202bef6d829ac859f7b61b3706601fcd4ca360421e54449c530eb7ca20e"
-              "e7dc0563cb0603d6c3ec1a9bc4ee9589f757a50f5abc48aa790e3dbac214719"
-              "7f")
+    url = ("https://raw.githubusercontent.com/asreview/asreview-covid19/master/datasets/cord19_v5_20191201.csv")  # noqa
+    sha512 = ("390c04b690abff2f824ed837367048308c6573032b2e45301056c3cfa7b04e6ea5b00ebd691e1af2066a820335bea1bdad240df736bd69be0b3c436fca629759")  # noqa
 
 
 class Covid19DataGroup(BaseDataGroup):
