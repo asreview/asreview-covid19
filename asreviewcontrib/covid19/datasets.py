a124ca625652feac490b846b3c389cba186cf125
qubixes <44498096+qubixes@users.noreply.github.com>
2020-04-18 00:55:12 +0200

    Allow for dynamic updates of the Covid dataset (#17)

--- asreviewcontrib/covid19/datasets.py
+++ asreviewcontrib/covid19/datasets.py
@@ -1,45 +1,19 @@
-from asreview.datasets import BaseDataSet
+from asreview.datasets import BaseDataSet, BaseVersionedDataSet
 from asreview.datasets import BaseDataGroup
 
 
-class Cord19Dataset(BaseDataSet):
-    dataset_id = "cord19"
-    authors = ["Allen institute for AI"]
-    title = "CORD-19"
-    topic = "Covid-19"
-    license = "Covid dataset license"
-    link = "https://pages.semanticscholar.org/coronavirus-research"
-    last_update = "2020-04-10"
-    description = "A free dataset on publications on the corona virus."
-    img_url = ("https://pages.semanticscholar.org/hs-fs/hubfs/"
-               "covid-image.png?width=300&name=covid-image.png")
-    link = "https://pages.semanticscholar.org/coronavirus-research"
-    year = 2020
+class Cord19Dataset(BaseVersionedDataSet):
+    base_url = "config/cord19-all"
+
+    def __init__(self):
+        super(Cord19Dataset, self).__init__("cord19", url=self.base_url)
 
 
-class Cord19DatasetV7(Cord19Dataset):
-    dataset_id = "cord19-v7"
-    title = "CORD-19 v7"
-    date = "2020-04-10"
-    statistics = {
-        "n_papers": 51078,
-        "n_missing_titles": 158,
-        "n_missing_abstracts": 8726,
-    }
-    url = ("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/metadata.csv")  # noqa
+class Cord19_2020Dataset(BaseVersionedDataSet):
+    base_url = "config/cord19-2020"
 
-
-class Cord19DatasetV7_Dec2019(Cord19Dataset):
-    dataset_id = "cord19-v7-2020"
-    title = "CORD-19 v7 since Dec. 2019"
-    last_update = "2020-04-10"
-    statistics = {
-        "n_papers": 5753,
-        "n_missing_titles": 2,
-        "n_missing_abstracts": 1422,
-    }
-    date = "2020-04-14"
-    url = ("https://raw.githubusercontent.com/asreview/asreview-covid19/master/datasets/cord19_v7_20191201.csv")  # noqa
+    def __init__(self):
+        super(Cord19Dataset, self).__init__("cord19-2020", url=self.base_url)
 
 
 class Covid19DataGroup(BaseDataGroup):
@@ -48,6 +22,6 @@ class Covid19DataGroup(BaseDataGroup):
 
     def __init__(self):
         super(Covid19DataGroup, self).__init__(
-            Cord19DatasetV7(),
-            Cord19DatasetV7_Dec2019(),
+            Cord19Dataset(),
+            Cord19_2020Dataset(),
         )
--- scripts/create_config.py
+++ scripts/create_config.py
@@ -1,41 +1,100 @@
 #!/usr/bin/env python
 
 import json
-import os
-from datetime import datetime, date
+from os.path import isfile
+from hashlib import sha512
+from pathlib import Path
+from urllib.request import urlretrieve
+
+from asreviewcontrib.statistics import DataStatistics
 
 
-def main():
-    config_fp = os.path.join("..", "asreviewcontrib", "covid19", "config.json")
-    covid_meta = {
-        "name": "cord19",
-        "title": "CORD-19",
-        "description": "A Free dataset on publications on the corona virus.",
-        "last_update": str(datetime.now()),
-        "license": "Covid dataset license",
-        "link": "https://pages.semanticscholar.org/coronavirus-research",
+def create_config(data_fp, last_update, complete=False, url=None):
+    if url is None:
+        url = f"https://raw.githubusercontent.com/asreview/asreview-covid19/master/datasets/{data_fp.name}"   # noqa
+    if complete:
+        config_fp = Path("..", "config", "cord19-all", data_fp.stem+".json")
+    else:
+        config_fp = Path("..", "config", "cord19-2020", data_fp.stem+".json")
+
+    with open(data_fp, "rb") as f:
+        data_sha512 = sha512(f.read()).hexdigest()
+
+    statistics = DataStatistics(data_fp).to_dict()
+    stat = {k: statistics[k] for k in
+            ["n_papers", "n_missing_title", "n_missing_abstract"]}
+    version = data_fp.stem.split("_")[1]
+
+    if complete:
+        title = f"CORD-19 {version}"
+        dataset_id = f"cord19-{version}"
+    else:
+        title = f"CORD-19 {version} since Dec. 2019"
+        dataset_id = f"cord19-2020-{version}"
+
+    config_data = {
+        "dataset_id": dataset_id,
+        "title": title,
+        "description": "A free dataset on publications on the corona virus.",
         "authors": ["Allen institute for AI"],
         "topic": "Covid-19",
-        "datasets": [{
-            "name": "metadata-2020-03-13",
-            "sha512": "6741211cc47c04897b253a3eaf2d18e6d57391530f8cebe7d8c84310f82"
-                      "c90b2c55071157b418fb7b627302adbfae8838fb8c071516288b320b131"
-                      "03ac1ec7fc",
-            "img_url": "https://pages.semanticscholar.org/hs-fs/hubfs/"
-                       "covid-image.png?width=300&name=covid-image.png",
-            "timestamp": str(date(year=2020, month=3, day=13)),
-            "statistics": {
-                "n_papers": 29500,
-                "n_missing_titles": 9,
-                "n_missing_abstracts": 2591,
-            },
-            "url": "https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/"
-                   "2020-03-13/all_sources_metadata_2020-03-13.csv"
-        }]
+        "link": "https://pages.semanticscholar.org/coronavirus-research",
+        "last_update": last_update,
+        "statistics": stat,
+        "img_url": ("https://pages.semanticscholar.org/hs-fs/hubfs/"
+                    "covid-image.png?width=300&name=covid-image.png"),
+        "license": "Covid dataset license",
+        "sha512": data_sha512,
+        "url": url,
     }
-    with open(config_fp, "w") as fp:
-        json.dump(covid_meta, fp, indent=4)
+    with open(config_fp, "w") as f:
+        json.dump(config_data, f, indent=4)
+
+
+def create_2020_configs():
+    base_dir = Path("..", "datasets")
+    datasets = [
+        (Path(base_dir, "cord19_v4_20191201.csv"), "2020-03-20"),
+        (Path(base_dir, "cord19_v5_20191201.csv"), "2020-03-27"),
+        (Path(base_dir, "cord19_v6_20191201.csv"), "2020-04-03"),
+        (Path(base_dir, "cord19_v7_20191201.csv"), "2020-04-10"),
+    ]
+    for data in datasets:
+        create_config(*data)
+
+    index_fp = Path("..", "config", "cord19-2020", "index.json")
+    with open(index_fp, "w") as f:
+        json.dump([x[0].stem + ".json" for x in datasets], f, indent=4)
+
+
+def create_complete_configs():
+    urls = [
+        ("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-13/all_sources_metadata_2020-03-13.csv",  #noqa
+         "v3", "2020-03-13"),
+        ("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-20/metadata.csv",  #noqa
+         "v4", "2020-03-20"),
+        ("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-27/metadata.csv",  #noqa
+         "v5", "2020-03-27"),
+        ("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-03/metadata.csv",  #noqa
+         "v6", "2020-04-03"),
+        ("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/metadata.csv",  #noqa
+         "v7", "2020-04-10"),
+    ]
+
+    data_dir = Path("..", "datasets")
+    data_names = []
+    for url, version, last_update in urls:
+        data_fp = Path(data_dir, f"cord19_{version}_all.csv")
+        data_names.append(data_fp.stem + ".json")
+        if not isfile(data_fp):
+            urlretrieve(url, data_fp)
+        create_config(data_fp, last_update=last_update, complete=True, url=url)
+
+    index_fp = Path("..", "config", "cord19-all", "index.json")
+    with open(index_fp, "w") as f:
+        json.dump([x for x in data_names], f, indent=4)
 
 
 if __name__ == "__main__":
-    main()
+    create_2020_configs()
+    create_complete_configs()
