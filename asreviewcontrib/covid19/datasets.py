18b236aacd9b284fc83fc1437ef2ab7df9f410f7
Raoul Schram <r.d.schram@uu.nl>
2020-04-18 11:09:17 +0200

    Fix it being local

--- asreviewcontrib/covid19/datasets.py
+++ asreviewcontrib/covid19/datasets.py
@@ -3,17 +3,19 @@ from asreview.datasets import BaseDataGroup
 
 
 class Cord19Dataset(BaseVersionedDataSet):
-    base_url = "config/cord19-all"
+    base_url = "https://raw.githubusercontent.com/asreview/asreview-covid19/master/config/cord19-all"
+    title = "CORD-19"
 
     def __init__(self):
         super(Cord19Dataset, self).__init__("cord19", url=self.base_url)
 
 
 class Cord19_2020Dataset(BaseVersionedDataSet):
-    base_url = "config/cord19-2020"
+    base_url = "https://raw.githubusercontent.com/asreview/asreview-covid19/master/config/cord19-2020"
+    title = "CORD-19 since Dec. 2019"
 
     def __init__(self):
-        super(Cord19Dataset, self).__init__("cord19-2020", url=self.base_url)
+        super(Cord19_2020Dataset, self).__init__("cord19-2020", url=self.base_url)
 
 
 class Covid19DataGroup(BaseDataGroup):
