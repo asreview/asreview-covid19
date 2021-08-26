f7575bcabb22101b37a7b09e03110f3da9b16e65
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-08-26 10:20:28 +0200

    Fix tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,12 +2,11 @@ from pytest import mark
 import urllib
 
 from asreview.datasets import get_available_datasets
-from asreview.datasets import find_data
+from asreview.datasets import DatasetManager
 
 
 ASREVIEW_COVID19_DATASET_COLLECTIONS = [
     "cord19-all",
-    "cord19-2020",
     "covid19-preprints"
 ]
 
@@ -20,20 +19,20 @@ def _dataset_url_exists(url):
     "data_name", ASREVIEW_COVID19_DATASET_COLLECTIONS
 )
 def test_find_datasets(data_name):
-    data = find_data(data_name)
+    data = DatasetManager().find(data_name)
 
     assert _dataset_url_exists(data)
 
 
 def test_get_available_datasets():
 
-    datasets = get_available_datasets()
+    datasets = DatasetManager().list('covid19')
 
     for key in ASREVIEW_COVID19_DATASET_COLLECTIONS:
 
-        assert key in datasets['covid19'].keys()
+        assert key in datasets.keys()
 
-        for dataset in datasets['covid19'][key].list():
+        for dataset in datasets[key].list():
             assert dataset.title is not None
             assert dataset.url is not None
             assert dataset.dataset_id is not None
