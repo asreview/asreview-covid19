6bb8981d695f91c2024c128f1f09bd4e9de34b06
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-12-12 22:39:46 +0100

    Make package ready for version 1.0 of ASReview LAB

--- asreviewcontrib/covid19/datasets.py
+++ asreviewcontrib/covid19/datasets.py
@@ -1,22 +1,30 @@
 import json
 from urllib.request import urlopen
 
-from asreview.datasets import BaseDataSet, BaseVersionedDataSet
+from asreview.datasets import BaseDataSet
 from asreview.datasets import BaseDataGroup
 
+try:
+    from asreview.datasets import BaseVersionedDataSet
 
-def dataset_from_meta(data):
-    if data["type"] == "versioned":
-        datasets = []
-        title = data["title"]
-        base_dataset_id = data["base_id"]
-        for config in data["configs"]:
-            datasets.append(BaseDataSet.from_config(config))
-        return BaseVersionedDataSet(base_dataset_id, datasets, title)
-    elif data["type"] == "base":
-        return BaseVersionedDataSet.from_config(data["configs"][0])
-    else:
-        raise ValueError(f"Dataset type {data['type']} unknown.")
+    def dataset_from_meta(data):
+        if data["type"] == "versioned":
+            datasets = []
+            title = data["title"]
+            base_dataset_id = data["base_id"]
+            for config in data["configs"]:
+                datasets.append(BaseDataSet.from_config(config))
+            return BaseVersionedDataSet(base_dataset_id, datasets, title)
+        elif data["type"] == "base":
+            return BaseVersionedDataSet.from_config(data["configs"][0])
+        else:
+            raise ValueError(f"Dataset type {data['type']} unknown.")
+
+    ASREVIEW_VERSION_1 = False
+
+except ImportError:
+    from asreview.datasets import _download_from_metadata
+    ASREVIEW_VERSION_1 = True
 
 
 class Covid19DataGroup(BaseDataGroup):
@@ -24,14 +32,18 @@ class Covid19DataGroup(BaseDataGroup):
     description = "A free dataset on publications on the corona virus."
 
     def __init__(self):
-        base_url = "https://raw.githubusercontent.com/asreview/asreview-covid19/master/config"  # noqa
-        meta_file = base_url + "/all.json"
-        with urlopen(meta_file, timeout=10) as f:
-            meta_data = json.loads(f.read().decode())
 
-        datasets = []
-        for data in meta_data.values():
-            datasets.append(dataset_from_meta(data))
+        if not ASREVIEW_VERSION_1:
+            meta_file = "https://raw.githubusercontent.com/asreview/asreview-covid19/master/config/all.json"
+            with urlopen(meta_file, timeout=10) as f:
+                meta_data = json.loads(f.read().decode())
+
+            datasets = []
+            for data in meta_data.values():
+                datasets.append(dataset_from_meta(data))
+        else:
+            meta_file = "https://raw.githubusercontent.com/asreview/asreview-covid19/master/config/index_v1.json"
+            datasets = _download_from_metadata(meta_file)
 
         super(Covid19DataGroup, self).__init__(
             *datasets
